3985e7b5e09f428fa683157236dab5a66186402c
Neo21 <tiwarinischae@gmail.com>
2022-06-13 14:13:12 -0400

    Forming arrays for the to-be-auctioned item names and their images

---
+++ src/arrayOfItems.java
@@ -0,0 +1,11 @@
+
+public class arrayOfItems {
+
+	public static void main(String[] args) {
+		
+		String[] itemNames = {"Anglo-Austrian Urn", "Silver Japanese Dining Goblet", "British Ballroom Gramophone", "Napoleon-era Parisian Mirror", "Al Capone's Bedside Telephone", "Gilded Porcelain Carousel", "Vintage Portuguese Lamp", "Thousand-year old Roman Clock", "Persian Astronomy Globe", "19th Century Chrysanthemum Oil Painting", "WW1-era British Vintage Car"};
+		String[] itemImages = {"ImagesFolder1/GlossaryPage.png"};
+
+	}
+
+}

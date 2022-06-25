012ddf66b4ff92914d661a2ae0588765e800d54b
Neo21 <tiwarinischae@gmail.com>
2022-06-24 23:21:39 -0400

    batchFileTest (2)

---
+++ bin/Culminating_FINAL_VERSION.bat
@@ -0,0 +1,28 @@
+javac TitlePage.java
+javac GameMode.java
+javac Glossary.java
+javac Rules.java
+javac MainMenu.java
+javac Player.java
+javac SinglePlayerDataInput.java
+javac MultiPlayerDataInput1.java
+javac MultiPlayerDataInput2.java
+javac ExceptionPage.java
+javac ItemDisplayPage.java
+javac MaxBidCalculator.java
+javac RoundWinPage1.java
+javac RoundWinPage2.java
+javac PlayerDataProperties1.java
+javac BiddingRoundComputer.java
+javac BiddingRoundSinglePlayer.java
+javac BiddingRoundPlayer1.java
+javac BiddingRoundPlayer2.java
+javac ComputerDataProperties.java
+javac EndScreen.java
+javac Loading.java
+javac PlayerDataProperties2.java
+javac SelectedAuctionItem.java
+javac SinglePlayerDataProperties.java
+
+pause
+java TitlePage
